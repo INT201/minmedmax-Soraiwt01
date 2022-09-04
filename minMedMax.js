68a5e4834585364a87109bc464cc9bba0588fbd1
Sorawit Taochoo <sorawit.tar@mail.kmutt.ac.th>
2022-09-04 18:30:54 +0700

    NoNo

--- minMedMax.js
+++ minMedMax.js
@@ -1,6 +1,32 @@
-const { template } = require('@babel/core')
+//const { template } = require('@babel/core')
 
 function minMedMax(n1, n2, n3) {
-  //code here
+  let Mnum=[n1,n2,n3];
+  if(n1<n3 && n1<n2 || n1 == n2){
+      if(n2<n3){
+        Mnum=[n1,n2,n3];
+      }
+      else{
+        Mnum=[n1,n3,n2];
+      }
+  }else if(n2<n1 && n2<n3){
+      if(n1<n3){
+        Mnum=[n2,n1,n3];
+      }
+      else{
+        Mnum=[n2,n3,n1];
+      }
+  }else{
+    if(n2<n1){
+      Mnum=[n3,n2,n1];
+    }
+    else{
+      Mnum=[n3,n1,n2];
+    }
+  }
+
+  return {min: Mnum[0],mid: Mnum[1],max: Mnum[2]};
+
 }
-module.exports = minMedMax
+
+//module.exports = minMedMax
