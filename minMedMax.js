2633227b1f1953749ffeba62ba87e5ddfe85f931
Sorawit Taochoo <sorawit.tar@mail.kmutt.ac.th>
2022-09-14 22:16:22 +0700

    abc

--- minMedMax.js
+++ minMedMax.js
@@ -6,16 +6,17 @@ function minMedMax(n1, n2, n3) {
 
   for(j = 1 ; j <= array.length ; j++){
     for(i = 1 ; i <= array.length ; i++) {
+        if(array[i-1]>array[i]){
         swap =array[i];// swap = 4
-        array[i] = array[i = 1]; // array[i -1]=5
-        array[i = 1] = swap ; // swap = 4
-
-    };
+        array[i] = array[i-1]; // array[i -1]=5
+        array[i-1] = swap ; // swap = 4
+      }
+    }
   }
 
-  return array;
+  return {min: array[0], mid: array[1], max: array[2]};
   }
 
 console.log(minMedMax(85, 30, 2));
-//dasdasdasd
+
 module.exports = minMedMax
